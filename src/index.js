fa085b918d266b51cb0ec3dfe7c563bb747ccd76
nghiatran <trannghia08032000@gmail.com>
2021-08-28 10:30:53 +0700

    add pug template

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const pug = require('pug');
 const port = 3000;
 
 app.use(morgan('combined'));
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-});
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'resources', 'views'));
+
+app.get('/', function(req, res){
+	res.render('index');
+})
 
 app.listen(port, () => {
   console.log(`http://localhost:${port}`);
