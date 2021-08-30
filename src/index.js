55e4e1034c6224a7afab1e592bbb157ac7dd3a9b
nghiatran <trannghia08032000@gmail.com>
2021-08-30 09:59:26 +0700

    change use template handlebars  express

--- src/index.js
+++ src/index.js
@@ -2,15 +2,30 @@ const path = require('path');
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
-const pug = require('pug');
+const handlebars = require('express-handlebars');
+const exphbs = require('express-handlebars');
+const methodOverride = require('method-override');
 const port = 3000;
 
 const route = require('./routes/index');
 
-app.use(morgan('combined'));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded());
+app.use(express.json());
 
-app.set('view engine', 'pug');
+//HTTP logger
+app.use(morgan('combined'));
+
+app.use(methodOverride('_method'));
+
+app.engine('hbs', handlebars({
+  extname: '.hbs',
+  helpers: {
+    
+  }
+}));
+
+app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources', 'views'));
 
 //route init
